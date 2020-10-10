14e4393f276f8e6c367cadb2c161ca7e2427ea9f
mustkem <mo.mustkem@gmail.com>
2020-10-10 13:36:45 +0530

    add

--- src/components/Root/Counter/Counter.js
+++ src/components/Root/Counter/Counter.js
@@ -41,6 +41,7 @@ const Counter = () => {
 
   return (
     <div>
+      Test
       <CounterValue data-testid="counter-value">Count: {counter}</CounterValue>
       <ButtonRow>
         <Button onClick={() => dispatch(-2)}>-2</Button>
